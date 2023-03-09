2f0d7a9554a1773efcd8215a064083d581330bb6
Strahinja Stanivuk <ssstanivuk@gmail.com>
2023-03-09 15:46:11 +0100

    Upsert liquidity pairs

--- src/modules/liquidity-pairs/liquidity-pairs.module.ts
+++ src/modules/liquidity-pairs/liquidity-pairs.module.ts
@@ -2,13 +2,20 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LiquidityPair } from './liquidity-pairs.entity';
+import { LiquidityPairsListener } from './liquidity-pairs.listener';
 import { LiquidityPairsMapper } from './liquidity-pairs.mapper';
+import { LiquidityPairsRepository } from './liquidity-pairs.repository';
 import { LiquidityPairsService } from './liquidity-pairs.service';
 
 @Module({
   imports: [HttpModule, TypeOrmModule.forFeature([LiquidityPair])],
   controllers: [],
-  providers: [LiquidityPairsService, LiquidityPairsMapper],
+  providers: [
+    LiquidityPairsService,
+    LiquidityPairsMapper,
+    LiquidityPairsRepository,
+    LiquidityPairsListener,
+  ],
   exports: [],
 })
 export class LiquidityPairsModule {}
---
+++ src/modules/liquidity-pairs/liquidity-pairs.repository.ts
@@ -0,0 +1,42 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { LiquidityPair } from './liquidity-pairs.entity';
+
+@Injectable()
+export class LiquidityPairsRepository {
+  private readonly logger = new Logger(LiquidityPairsRepository.name);
+
+  constructor(
+    @InjectRepository(LiquidityPair)
+    private readonly repository: Repository<LiquidityPair>,
+  ) {}
+
+  public async upsertAll(liquidityPairs: LiquidityPair[]) {
+    liquidityPairs.forEach((liquidityPair) => {
+      this.upsert(liquidityPair);
+    });
+  }
+
+  private async upsert(liquidityPair: LiquidityPair) {
+    liquidityPair.updatedAt = new Date();
+
+    const existingPair = await this.repository.findOneBy({
+      baseAsset: liquidityPair.baseAsset,
+      token: liquidityPair.token,
+    });
+
+    if (!existingPair) {
+      this.repository.insert(liquidityPair);
+      return;
+    }
+
+    this.repository.update(
+      {
+        baseAsset: liquidityPair.baseAsset,
+        token: liquidityPair.token,
+      },
+      liquidityPair,
+    );
+  }
+}
