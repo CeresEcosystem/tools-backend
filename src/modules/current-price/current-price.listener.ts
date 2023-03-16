c993c153f4fb531df987353fafc3090a5adb2cb5
Danijel Radulović <master.daca09@gmail.com>
2023-03-16 12:07:44 +0100

    Migrate prices and pairs logic from ceres-locker-backend

--- src/modules/current-price/current-price.listener.ts
+++ src/modules/current-price/current-price.listener.ts
@@ -1,34 +1,99 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { firstValueFrom } from 'rxjs';
+import { ApiPromise } from '@polkadot/api/promise';
+import { WsProvider } from '@polkadot/rpc-provider';
+import { options } from '@sora-substrate/api';
+import { PROVIDER } from '../../constants/constants';
 import { CronExpression } from 'src/utils/cron-expression.enum';
 import { CurrentPriceBcDto } from './dto/current-price-bc.dto';
 import { CurrentPriceService } from './current-price.service';
+import * as whitelist from 'src/utils/files/whitelist.json';
+import { FPNumber } from '@sora-substrate/math';
 
-const TOKEN_PRICES_URL = 'https://api.cerestoken.io/prices';
+const DAI_ADDRESS =
+  '0x0200060000000000000000000000000000000000000000000000000000000000';
+const HUNDREDS = ['HOT', 'UMI', 'SOSHIBA'];
+const BILLIONS = ['MEOW'];
 
 @Injectable()
 export class CurrentPriceListener {
   private readonly logger = new Logger(CurrentPriceListener.name);
+  private soraApi;
+  private tokens;
 
-  constructor(
-    private readonly currentPriceService: CurrentPriceService,
-    private readonly httpService: HttpService,
-  ) {}
+  constructor(private readonly currentPriceService: CurrentPriceService) {
+    const provider = new WsProvider(PROVIDER);
+    new ApiPromise(options({ provider, noInitWarn: true })).isReady.then(
+      (api) => {
+        this.soraApi = api;
+        this.getTokens().then();
+      },
+    );
+  }
 
-  // TODO: Phase II - Refactor this class to listen to changes on BC.
+  async getTokens(): Promise<void> {
+    this.tokens = new Map();
+    let tokens = await this.soraApi.query.assets.assetInfos.entries();
+    for (let [assetId, token] of tokens) {
+      assetId = assetId.toHuman()[0].code;
+      if (!whitelist.includes(assetId)) {
+        continue;
+      }
+      token = token.toHuman();
+      let assetSymbol = token[0];
+      let fullName = token[1] + ' (' + assetSymbol + ')';
+      this.tokens.set(assetSymbol, {
+        asset_id: assetId,
+        full_name: fullName,
+      });
+    }
+  }
 
   @Cron(CronExpression.EVERY_MINUTE)
   async fetchTokenPrices(): Promise<void> {
-    this.logger.log('Start downloading token prices.');
+    this.logger.log('Start fetching tokens prices.');
+    let prices: CurrentPriceBcDto[] = [];
+    for (let entry of this.tokens) {
+      let symbol = entry[0];
+      let token = entry[1];
+      let amount = 1;
 
-    const { data } = await firstValueFrom(
-      this.httpService.get<CurrentPriceBcDto[]>(TOKEN_PRICES_URL),
-    );
+      if (HUNDREDS.includes(symbol)) {
+        amount = 100;
+      } else if (BILLIONS.includes(symbol)) {
+        amount = 1000000000;
+      }
 
-    this.currentPriceService.save(data);
+      await this.soraApi.rpc.liquidityProxy.quote(
+        0,
+        token['asset_id'],
+        DAI_ADDRESS,
+        FPNumber.fromNatural(amount).bnToString(),
+        'WithDesiredInput',
+        ['XYKPool'],
+        'Disabled',
+        (result) => {
+          const value = !result.isNone
+            ? result.unwrap()
+            : { amount: 0, fee: 0, rewards: [], amountWithoutImpact: 0 };
+          let price: any = new FPNumber(value.amount).toNumber();
+          price = HUNDREDS.includes(symbol) ? (price / 100).toFixed(8) : price;
+          price = BILLIONS.includes(symbol)
+            ? (price / 1000000000).toFixed(12)
+            : price;
 
-    this.logger.log('Downloading of token prices was successful!');
+          prices.push({
+            token: symbol.toUpperCase(),
+            price: symbol === 'DAI' ? '1' : price.toString(),
+            asset_id: token['asset_id'],
+            full_name: token['full_name'],
+          });
+        },
+      );
+    }
+
+    this.currentPriceService.save(prices);
+
+    this.logger.log('Fetching of tokens prices was successful!');
   }
 }
