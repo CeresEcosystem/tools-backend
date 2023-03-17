7c32e5dad5729d9f583fb68b82a1e57305d8ddfb
StraleCBS <stralegit@gmail.com>
2023-03-17 20:32:35 +0100

    Add retry for farming rewards

--- src/modules/rewards/rewards.sync.ts
+++ src/modules/rewards/rewards.sync.ts
@@ -1,7 +1,8 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { firstValueFrom } from 'rxjs';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom, of, retry } from 'rxjs';
 import { CronExpression } from 'src/utils/cron-expression.enum';
 import { RewardsDto } from './rewards.dto';
 import { RewardsService } from './rewards.service';
@@ -22,8 +23,20 @@ export class RewardsSync {
     this.logger.log('Start downloading farming rewards.');
 
     const { data: rewards } = await firstValueFrom(
-      this.httpService.get<RewardsDto>(LP_URL),
+      this.httpService.get<RewardsDto>(LP_URL, { timeout: 2000 }).pipe(
+        retry({ count: 30, delay: 1000 }),
+        catchError((error: AxiosError) => {
+          this.logger.warn(
+            `An error happened while fetching farming rewards! msg: ${error.message}, code: ${error.code}, cause: ${error.cause}`,
+          );
+          return of({ data: undefined });
+        }),
+      ),
     );
+
+    if (!rewards) {
+      return;
+    }
 
     this.rewardsService.save(rewards);
 
