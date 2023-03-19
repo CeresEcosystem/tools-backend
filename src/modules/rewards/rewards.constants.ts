c00ece4dcff79d56a5c674cf061e3ebab8b69305
Strahinja Stanivuk <ssstanivuk@gmail.com>
2023-03-19 08:50:43 +0100

    Remove redundant keys

--- src/modules/rewards/rewards.constants.ts
+++ src/modules/rewards/rewards.constants.ts
@@ -1,7 +1,5 @@
 export const FARMING_APR_KEY = 'farming-apr';
 export const FARMING_REWARDS_KEY = 'farming-rewards';
-export const FARMING_APR_DOUBLE_KEY = 'farming-apr-double';
-export const FARMING_REWARDS_DOUBLE_KEY = 'farming-rewards-double';
 
 export const CACHE_KEYS = {
   REWARDS: 'rewards',
--- src/modules/rewards/rewards.sync.ts
+++ src/modules/rewards/rewards.sync.ts
@@ -1,4 +1,3 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { CronExpression } from 'src/utils/cron-expression.enum';
@@ -6,47 +5,42 @@ import { RewardsService } from './rewards.service';
 import { PairsService } from '../pairs/pairs.service';
 import { TokenPriceService } from '../token-price/token-price.service';
 
+const DOUBLE_POOLS = ['DAI', 'PSWAP', 'ETH', 'VAL', 'XST'];
+
 @Injectable()
 export class RewardsSync {
   private readonly logger = new Logger(RewardsSync.name);
 
   constructor(
-    private readonly httpService: HttpService,
     private readonly tokenPriceService: TokenPriceService,
     private readonly pairsService: PairsService,
     private readonly rewardsService: RewardsService,
   ) {}
 
   @Cron(CronExpression.EVERY_5_MINUTES)
-  async fetchFarmingRewards(): Promise<void> {
+  async calculateFarmingRewards(): Promise<void> {
     this.logger.log('Start calculating farming rewards.');
+
     const pairs = await this.pairsService.findAll();
     const { price: xorPrice } = await this.tokenPriceService.findByToken('XOR');
     const { price: pswapPrice } = await this.tokenPriceService.findByToken(
       'PSWAP',
     );
-    const doublePools = ['DAI', 'PSWAP', 'ETH', 'VAL', 'XST'];
 
     let baseAssetLiquidity = 0;
-    for (let pair of pairs) {
-      if (pair.baseAsset === 'XOR' && doublePools.includes(pair.token)) {
+
+    pairs.forEach((pair) => {
+      if (pair.baseAsset === 'XOR' && DOUBLE_POOLS.includes(pair.token)) {
         baseAssetLiquidity += pair.baseAssetLiq;
       }
-    }
+    });
 
     const pswapRewards = 2500000 / baseAssetLiquidity;
     const apr =
       (365 * 2500000 * parseFloat(pswapPrice) * 100) /
       (baseAssetLiquidity * parseFloat(xorPrice) * 2);
 
-    const rewards = {
-      apr: apr.toFixed(2),
-      rewards: pswapRewards.toFixed(2),
-      aprDouble: (apr * 2).toFixed(2),
-      rewardsDouble: (pswapRewards * 2).toFixed(2),
-    };
-
-    this.rewardsService.save(rewards);
+    this.rewardsService.save(apr.toFixed(2), pswapRewards.toFixed(2));
 
     this.logger.log('Calculating of farming rewards was successful!');
   }
