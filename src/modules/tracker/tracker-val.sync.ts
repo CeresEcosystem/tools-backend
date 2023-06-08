9592d5b4e8b948af9d7f96a4c70ebea648421147
Danijel Radulović <master.daca09@gmail.com>
2023-06-08 23:58:43 +0200

    Implement VAL tracker sync

--- src/modules/tracker/tracker-val.sync.ts
+++ src/modules/tracker/tracker-val.sync.ts
@@ -4,24 +4,83 @@ import { CronExpression } from 'src/utils/cron-expression.enum';
 import { TrackerService } from './tracker.service';
 import { VALTrackerBlockBcToEntityMapper } from './mapper/val-tracker-block-bc-to-entity.mapper';
 import { ValTrackerBlockDto } from './dto/val-tracker-bc-block';
+import { WsProvider } from '@polkadot/rpc-provider';
+import { PROVIDER } from '../../constants/constants';
+import { ApiPromise } from '@polkadot/api/promise';
+import { options } from '@sora-substrate/api';
+import { FPNumber } from '@sora-substrate/math';
+import { DENOMINATOR } from './tracker.constants';
+
+const techAccount = 'cnTQ1kbv7PBNNQrEb1tZpmK7hhnohXfYrx5GuD1H9ShjdGoBh';
 
 @Injectable()
 export class TrackerValSync {
   private readonly logger = new Logger(TrackerValSync.name);
+  private soraApi;
 
   constructor(
     private readonly trackerService: TrackerService,
     private readonly mapper: VALTrackerBlockBcToEntityMapper,
-  ) {}
+  ) {
+    const provider = new WsProvider(PROVIDER);
+    new ApiPromise(options({ provider, noInitWarn: true })).isReady.then(
+      (api) => {
+        this.soraApi = api;
+      },
+    );
+  }
 
-  @Cron(CronExpression.EVERY_10_MINUTES, { disabled: true })
+  @Cron(CronExpression.EVERY_6_HOURS)
   async fetchTrackerData(): Promise<void> {
-    this.logger.log('Start fetching burning data.');
+    this.logger.log('Start fetching VAL burning data.');
 
     const burningData: ValTrackerBlockDto[] = [];
-    // const startBlock = await this.trackerService.findLastBlockNumber('VAL');
+    const startBlock = await this.trackerService.findLastBlockNumber('VAL');
+    const headBlock = await this.soraApi.query.system.number();
 
-    //TODO: implement VAL burning data sync
+    for (let blockNum = startBlock; blockNum <= headBlock; blockNum++) {
+      const blockHash = await this.soraApi.rpc.chain.getBlockHash(blockNum);
+      const apiAt = await this.soraApi.at(blockHash);
+      let events = await apiAt.query.system.events();
+      let found = false;
+      events = events.toHuman();
+
+      events.forEach((e, _) => {
+        const module = e.event.section;
+        const event = e.event.method;
+        if (module === 'session' && event === 'NewSession') {
+          events.forEach((ev, iddx) => {
+            if (found) return;
+            const module = ev.event.section;
+            const event = ev.event.method;
+            if (module === 'balances' && event === 'Deposit') {
+              const accountId = ev.event.data.who;
+              if (accountId === techAccount) {
+                const xorDedicatedForBuyBack = new FPNumber(
+                  ev.event.data.amount,
+                )
+                  .div(DENOMINATOR)
+                  .toString();
+                const valBurned = new FPNumber(events[iddx + 7].event.data[3])
+                  .div(DENOMINATOR)
+                  .toNumber();
+                const valRemintedParliament = (valBurned * 0.1).toString();
+                found = true;
+
+                burningData.push({
+                  dateRaw: '',
+                  blockNum,
+                  valBurned: valBurned.toString(),
+                  valRemintedParliament,
+                  xorDedicatedForBuyBack,
+                  xorTotalFee: xorDedicatedForBuyBack,
+                });
+              }
+            }
+          });
+        }
+      });
+    }
 
     if (!burningData || burningData.length === 0) {
       this.logger.log('No new VAL burning data to load, exiting.');
@@ -32,6 +91,6 @@ export class TrackerValSync {
 
     await this.trackerService.insert(this.mapper.toEntities(burningData));
 
-    this.logger.log('Fetching of burning data was successful!');
+    this.logger.log('Fetching of VAL burning data was successful!');
   }
 }
