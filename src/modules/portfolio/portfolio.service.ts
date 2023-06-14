0cd6b721db8a5f199e6305fc4e518532143b6395
Danijel Radulović <master.daca09@gmail.com>
2023-06-14 12:38:15 +0200

    Revert "Remove url logic"
    This reverts commit 5611bd60c836378ef93bf834fe56835fa52027cd.

--- src/modules/portfolio/portfolio.service.ts
+++ src/modules/portfolio/portfolio.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { Injectable, Logger } from '@nestjs/common';
 import { WsProvider, ApiPromise } from '@polkadot/api';
 import { FPNumber } from '@sora-substrate/math';
 import { XOR_ADDRESS, PROVIDER } from 'src/constants/constants';
@@ -8,6 +10,8 @@ import { TokenPriceService } from '../token-price/token-price.service';
 import { ChronoPriceService } from '../chrono-price/chrono-price.service';
 
 const DENOMINATOR = FPNumber.fromNatural(Math.pow(10, 18));
+const URL =
+  'https://data.cerestoken.io/api/trading/history?symbol=XOR&resolution=30&';
 const intervals = [2, 48, 336, 1440];
 
 @Injectable()
@@ -17,6 +21,7 @@ export class PortfolioService {
   constructor(
     private tokenPriceService: TokenPriceService,
     private chronoPriceService: ChronoPriceService,
+    private httpService: HttpService,
   ) {
     const provider = new WsProvider(PROVIDER);
     new ApiPromise(options({ provider, noInitWarn: true })).isReady.then(
@@ -24,10 +29,16 @@ export class PortfolioService {
     );
   }
 
+  async fetchPricesForInterval(url) {
+    const { data } = await firstValueFrom(this.httpService.get<any>(url));
+    return data;
+  }
+
   async getPortfolio(accountId: string): Promise<PortfolioDto[]> {
     const timestamp = Math.floor(Date.now() / 1000);
     const timestampBefore30Days = timestamp - 2592000;
 
+    // const URL_XOR = `${URL}from=${timestampBefore30Days}&to=${timestamp}`;
     let assetIdsAndAssetBalances: PortfolioDto[] = [];
 
     const xor = await this.api.rpc.assets.freeBalance(accountId, XOR_ADDRESS);
@@ -35,6 +46,7 @@ export class PortfolioService {
     const balance = new FPNumber(value.balance).toNumber();
     const tokenEntity = await this.tokenPriceService.findByAssetId(XOR_ADDRESS);
 
+    // const { o: prices } = await this.fetchPricesForInterval(URL_XOR);
     const { o: prices } = await this.chronoPriceService.getPriceForChart(
       tokenEntity.token,
       30,
@@ -70,6 +82,9 @@ export class PortfolioService {
       let [, { code: assetId }] = assetsId.toHuman();
       let tokenEntity = await this.tokenPriceService.findByAssetId(assetId);
 
+      // let urlTokens = `https://data.cerestoken.io/api/trading/history?symbol=${tokenEntity.token}&resolution=30&from=${timestampBefore30Days}&to=${timestamp}`;
+
+      // const { o: prices } = await this.fetchPricesForInterval(urlTokens);\
       const { o: prices } = await this.chronoPriceService.getPriceForChart(
         tokenEntity.token,
         30,
