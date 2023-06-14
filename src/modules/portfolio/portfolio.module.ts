aa3df59993256bb7522869467ffe735ca902faa6
Ognjen Aleksic <ognjen@ceresblockchain.solutions>
2023-06-14 15:12:58 +0200

    Remove url and constants file

--- src/modules/portfolio/portfolio.module.ts
+++ src/modules/portfolio/portfolio.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { PortfolioController } from './portfolio.controller';
 import { PortfolioService } from './portfolio.service';
 import { TokenPriceModule } from '../token-price/token-price.module';
 import { ChronoPriceModule } from '../chrono-price/chrono-price.module';
 
 @Module({
-  imports: [HttpModule, TokenPriceModule, ChronoPriceModule],
+  imports: [TokenPriceModule, ChronoPriceModule],
   controllers: [PortfolioController],
   providers: [PortfolioService],
 })
