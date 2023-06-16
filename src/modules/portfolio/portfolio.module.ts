f75f70873374cb8ee6155ca5bc79333640795782
Ognjen Aleksic <ognjen@ceresblockchain.solutions>
2023-06-16 12:20:08 +0200

    Add staking portfolio

--- src/modules/portfolio/portfolio.module.ts
+++ src/modules/portfolio/portfolio.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { PortfolioController } from './portfolio.controller';
 import { PortfolioService } from './portfolio.service';
 import { TokenPriceModule } from '../token-price/token-price.module';
 import { ChronoPriceModule } from '../chrono-price/chrono-price.module';
 
 @Module({
-  imports: [TokenPriceModule, ChronoPriceModule],
+  imports: [HttpModule, TokenPriceModule, ChronoPriceModule],
   controllers: [PortfolioController],
   providers: [PortfolioService],
 })
