43ba0eec6bc2ed31cd3d31517fb8487b1c3ad5c3
Danijel Radulović <master.daca09@gmail.com>
2023-10-13 16:20:40 +0200

    Revert "\"
    This reverts commit 53cc4d8a83936ff234a12f29c8d82d76558a489f.

--- src/modules/pairs-liquidity/dto/pair-liquidity-change-data.dto.ts
+++
@@ -1,9 +0,0 @@
-import { TransactionType } from '../entity/pair-liquidity-change.entity';
-
-export interface PairLiquidityChangeDataDto {
-  transactionType: TransactionType;
-  signerId: string;
-  blockNumber: number;
-  timestamp: number;
-  eventArgs: any;
-}
