ec2aeef12c69352a83c933047985ebe034573ce2
Srdjan Djukanovic <116005505+sdjuka@users.noreply.github.com>
2023-12-25 16:48:12 +0100

    Add post method for fetching swaps by tokens

--- src/modules/swaps/swaps.controller.ts
+++ src/modules/swaps/swaps.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { SwapService } from './swaps.service';
 import { PageDto } from 'src/utils/pagination/page.dto';
 import { PageOptionsDto } from 'src/utils/pagination/page-options.dto';
 import { SwapDto } from './dto/swap.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { SwapOptionsDto } from './dto/swap-options.dto';
+import { SwapTokensDto } from './dto/swap-tokens.dto';
 
 @Controller('swaps')
 @ApiTags('Swaps controller')
@@ -23,6 +24,19 @@ export class SwapsController {
       pageOptions,
       swapOptions,
       tokensArr,
+    );
+  }
+
+  @Post()
+  public getSwapsForTokens(
+    @Body() swapTokens: SwapTokensDto,
+    @Query() pageOptions: PageOptionsDto,
+    @Query() swapOptions: SwapOptionsDto,
+  ): Promise<PageDto<SwapDto>> {
+    return this.swapService.findSwapsByTokens(
+      pageOptions,
+      swapOptions,
+      swapTokens.tokens,
     );
   }
 
