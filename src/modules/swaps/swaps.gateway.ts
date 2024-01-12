bd8e5b2634dc57bdefe54e955a8144d30e6cb629
StraleCBS <stralegit@gmail.com>
2024-01-12 22:10:46 +0100

    Log WebSocket uninitialized error

--- src/modules/swaps/swaps.gateway.ts
+++ src/modules/swaps/swaps.gateway.ts
@@ -6,14 +6,23 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { SwapDto } from './dto/swap.dto';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ namespace: 'swapsocket', cors: { origin: '*' } })
 export class SwapGateway {
+  private readonly logger = new Logger(SwapGateway.name);
+
   @WebSocketServer()
   server: Server;
 
   @SubscribeMessage('newSwap')
   onSwap(@MessageBody() swap: SwapDto): void {
+    if (!this.server) {
+      this.logger.error('WebSocketServer uninitialized');
+
+      return;
+    }
+
     this.server.emit(swap.inputAssetId, swap);
     this.server.emit(swap.outputAssetId, swap);
   }
