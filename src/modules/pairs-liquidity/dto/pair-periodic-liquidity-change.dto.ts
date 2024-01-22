0db5960434b162c8498481d7e377ae5862ae482f
Srdjan Djukanovic <116005505+sdjuka@users.noreply.github.com>
2024-01-22 12:14:41 +0100

    Fix PairPeriodicLiquidityChange dto

--- src/modules/pairs-liquidity/dto/pair-periodic-liquidity-change.dto.ts
+++ src/modules/pairs-liquidity/dto/pair-periodic-liquidity-change.dto.ts
@@ -2,5 +2,7 @@ export interface PairPeriodicLiquidityChangeDto {
   baseAssetSymbol: string;
   tokenAssetSymbol: string;
   liquidity: number;
+  baseAssetLiq: number;
+  tokenAssetLiq: number;
   updatedAt: Date;
 }
