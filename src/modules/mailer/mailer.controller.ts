6c2204dff023c65489cae1ab93177a0fa716c148
StraleCBS <stralegit@gmail.com>
2024-01-23 08:03:43 +0100

    Revert "Disable node mailer"
    This reverts commit 396f747bfde069455a49c3050ea112adbcf3348c.

--- src/modules/mailer/mailer.controller.ts
+++ src/modules/mailer/mailer.controller.ts
@@ -1,39 +1,39 @@
-// import { MailerService } from '@nestjs-modules/mailer';
-// import {
-//   BadGatewayException,
-//   Body,
-//   Controller,
-//   Logger,
-//   Post,
-// } from '@nestjs/common';
-// import { ApiTags } from '@nestjs/swagger';
-// import { ContactUsDto } from './contact-us.dto';
+import { MailerService } from '@nestjs-modules/mailer';
+import {
+  BadGatewayException,
+  Body,
+  Controller,
+  Logger,
+  Post,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ContactUsDto } from './contact-us.dto';
 
-// @Controller('emails')
-// @ApiTags('Mailer Controller')
-// export class MailerController {
-//   private readonly logger = new Logger(MailerController.name);
+@Controller('emails')
+@ApiTags('Mailer Controller')
+export class MailerController {
+  private readonly logger = new Logger(MailerController.name);
 
-//   constructor(private readonly mailerService: MailerService) {}
+  constructor(private readonly mailerService: MailerService) {}
 
-//   @Post('/contact-us')
-//   public async sendContactUsEmail(
-//     @Body() contactUsDto: ContactUsDto,
-//   ): Promise<void> {
-//     const emailBody =
-//       'Contact form data: \n' +
-//       `Name: ${contactUsDto.name} \n` +
-//       `Email: ${contactUsDto.email} \n` +
-//       `Message: ${contactUsDto.message}`;
+  @Post('/contact-us')
+  public async sendContactUsEmail(
+    @Body() contactUsDto: ContactUsDto,
+  ): Promise<void> {
+    const emailBody =
+      'Contact form data: \n' +
+      `Name: ${contactUsDto.name} \n` +
+      `Email: ${contactUsDto.email} \n` +
+      `Message: ${contactUsDto.message}`;
 
-//     await this.mailerService
-//       .sendMail({
-//         replyTo: contactUsDto.email,
-//         text: emailBody,
-//       })
-//       .catch((error) => {
-//         this.logger.error(`${error.message}\nRequest: ${emailBody}`);
-//         throw new BadGatewayException('Failed to send the email');
-//       });
-//   }
-// }
+    await this.mailerService
+      .sendMail({
+        replyTo: contactUsDto.email,
+        text: emailBody,
+      })
+      .catch((error) => {
+        this.logger.error(`${error.message}\nRequest: ${emailBody}`);
+        throw new BadGatewayException('Failed to send the email');
+      });
+  }
+}
