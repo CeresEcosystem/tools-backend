cef247332bc7c59531949f3ed882fd222c3ccae0
Srdjan Djukanovic <116005505+sdjuka@users.noreply.github.com>
2024-03-14 09:15:25 +0100

    Add caching

---
+++ src/modules/pairs-liquidity/pair-liquidity.constants.ts
@@ -0,0 +1,7 @@
+export const CACHE_KEYS = {
+  PROVIDERS: 'providers',
+};
+
+export const CACHE_TTL = {
+  ONE_HOUR: 3600000,
+};
--- src/modules/pairs-liquidity/pairs-liquidity.controller.ts
+++ src/modules/pairs-liquidity/pairs-liquidity.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { PageOptionsDto } from 'src/utils/pagination/page-options.dto';
 import { PageDto } from 'src/utils/pagination/page.dto';
@@ -6,12 +6,17 @@ import { PairLiquidityChangeDto } from './dto/pair-liquidity-change.dto';
 import { PairsLiquidityService } from './pairs-liquidity.service';
 import { PairPeriodicLiquidityChangeDto } from './dto/pair-periodic-liquidity-change.dto';
 import { PairLiquidityProviderDto } from './dto/pair-liquidity-provider.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { CACHE_KEYS, CACHE_TTL } from './pair-liquidity.constants';
+import { Cache } from 'cache-manager';
 
 @Controller('pairs-liquidity')
 @ApiTags('Pairs Liquidity Controller')
 export class PairsLiquidityController {
   constructor(
     private readonly pairsLiquidityChangesService: PairsLiquidityService,
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
   ) {}
 
   @Get('/:assetA/:assetB')
@@ -39,9 +44,14 @@ export class PairsLiquidityController {
     @Param('baseAsset') baseAsset: string,
     @Param('tokenAsset') tokenAsset: string,
   ): Promise<PairLiquidityProviderDto[]> {
-    return this.pairsLiquidityChangesService.getLiquidityProviders(
-      baseAsset,
-      tokenAsset,
+    return this.cacheManager.wrap(
+      `${CACHE_KEYS.PROVIDERS}-${baseAsset}-${tokenAsset}`,
+      () =>
+        this.pairsLiquidityChangesService.getLiquidityProviders(
+          baseAsset,
+          tokenAsset,
+        ),
+      CACHE_TTL.ONE_HOUR,
     );
   }
 }
